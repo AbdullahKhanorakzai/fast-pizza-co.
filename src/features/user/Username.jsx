8834d29fb8a9370a04cfcbed939bfe5a7cde8121
abdullah <abdullahkhanorakzai@gmail.com>
2024-02-01 23:58:39 +0500

    add username to mobail view

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -5,9 +5,7 @@ function Username() {
 
   if (!userName) return null;
 
-  return (
-    <div className="hidden text-sm font-semibold md:block">{userName}</div>
-  );
+  return <div className=" text-sm font-semibold md:block">{userName}</div>;
 }
 
 export default Username;
