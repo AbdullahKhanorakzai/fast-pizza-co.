cd54b7296d3f370dbc9eb6c81d251012dcb879de
abdullah <abdullahkhanorakzai@gmail.com>
2024-02-02 17:13:30 +0500

    style cart

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -10,7 +10,7 @@ function CartOverview() {
   if (!toatalCartQuantity) return null;
 
   return (
-    <div className="rou flex items-center justify-between rounded-t-lg bg-stone-800 px-4 py-5 uppercase text-stone-200 drop-shadow-md sm:px-6 sm:drop-shadow-none">
+    <div className="rou flex items-center justify-between rounded-t-xl bg-stone-800 px-4 py-5 uppercase text-stone-200 drop-shadow-xl sm:px-6 sm:drop-shadow-none">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6 sm:text-sm md:text-base">
         <span>{toatalCartQuantity} pizzas</span>
         <span>{formatCurrency(toatalCartPrice)}</span>
